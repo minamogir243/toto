dcf51b943b8fe0992f189009f5c3cd9820a4c079
minamogir243 <168736987+minamogir243@users.noreply.github.com>
2024-08-28 13:43:58 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// リダイレクト先のURLを指定
+const redirectUrl = "https://sites.google.com/view/addonsyoji/%E3%83%9B%E3%83%BC%E3%83%A0";  // リダイレクト先のURLを指定してください
+
+// 3秒後にリダイレクト
+setTimeout(() => {
+    window.location.href = redirectUrl;
+}, 3000);
